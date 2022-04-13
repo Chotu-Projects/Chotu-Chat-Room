e6829988cc131cf40884d4cc4b2165d66393da8d
Punit Choudhary <itspunitchoudhary@gmail.com>
2022-04-13 20:07:46 +0530

    add localhost url

--- server.js
+++ server.js
@@ -72,4 +72,4 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port http://127.0.0.1:${PORT}`));
